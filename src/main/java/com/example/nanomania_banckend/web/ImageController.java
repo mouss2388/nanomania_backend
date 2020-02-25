eac77a73959a1a98b8d058a0d76992f3d2e9a296
mouss <mouss2388@gmail.com>
2020-02-25 20:46:41 +0100

    ISSUE GET THUMB FILE

--- src/main/java/com/example/nanomania_banckend/web/ImageController.java
+++ src/main/java/com/example/nanomania_banckend/web/ImageController.java
@@ -9,6 +9,10 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -29,6 +33,7 @@ import com.example.nanomania_banckend.models.VideoGame;
 import com.example.nanomania_banckend.repositories.ImageRepository;
 import com.example.nanomania_banckend.repositories.VideoGameRepository;
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.gridfs.model.GridFSFile;
 
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.resizers.configurations.ScalingMode;
@@ -36,7 +41,7 @@ import net.coobird.thumbnailator.resizers.configurations.ScalingMode;
 @Controller
 @RequestMapping("/images")
 public class ImageController {
-	
+
 	private static final int THUMB_WIDTH = 256;
 	private static final int THUMB_HEIGHT = 256;
 	@Autowired
@@ -44,56 +49,42 @@ public class ImageController {
 	@Autowired
 	private ImageRepository imageRepository;
 
+	@Autowired
+	private VideoGameRepository videoGameRepository;
+
+	
 	@ResponseBody
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public Iterable<Image> listeImages(){
 		return this.imageRepository.findAll();
-		
+
 	}
-	
-	@Autowired
-	private VideoGameRepository videoGameRepository;
-	
-//	@PostMapping("/upload")
-//	public ResponseEntity<Image> upload(@RequestParam("file") MultipartFile file,
-//										@RequestParam("gameId") int gameId){
-//		Optional<VideoGame> g = videoGameRepository.findById(gameId);
-//		if (!g.isPresent())
-//			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//		try {
-//			Image img = new Image(0,
-//					file.getOriginalFilename(),
-//					file.getOriginalFilename(),
-//					file.getContentType());
-//			// sauvegarde fichier et generation thumbnail
-//			imageRepository.saveImageFile(img, file.getInputStream());
-//			img.setVideoGame(g.get());
-//			img = imageRepository.save(img);
-//			return new ResponseEntity<Image>(img, HttpStatus.CREATED);
-//		} catch (IOException e) {e.printStackTrace();}
-//		return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//	}
-	
-	
-	
+
+
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<Image> uploadFile(@RequestParam("file") MultipartFile file,
-			@RequestParam("description") String description) throws IOException {
-	
+			@RequestParam("description") String description,
+			@RequestParam("gameId") int gameId) throws IOException {
+
+		Optional<VideoGame> g = videoGameRepository.findById(gameId);
+		if (!g.isPresent())
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);		
+		
 		ObjectId thumbId = null;
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		
 		try {
 			Thumbnails.of(file.getInputStream())
-					  .size(THUMB_WIDTH, THUMB_HEIGHT)
-					  .scalingMode(ScalingMode.BICUBIC)
-					  .outputFormat("jpg")
-					  .toOutputStream(bos);
+			.size(THUMB_WIDTH, THUMB_HEIGHT)
+			.scalingMode(ScalingMode.BICUBIC)
+			.outputFormat("jpg")
+			.toOutputStream(bos);
 			thumbId = this.gridFsTemplate.store(new ByteArrayInputStream(bos.toByteArray()),
 					"thumb_" + file.getOriginalFilename(),
 					file.getContentType(),
 					new BasicDBObject("description", description));
-			
+
 		} catch (IOException | ArrayIndexOutOfBoundsException e) {
 			return new ResponseEntity<>( HttpStatus.NOT_IMPLEMENTED);
 		}
@@ -102,52 +93,95 @@ public class ImageController {
 				file.getContentType(),
 				new BasicDBObject("description", description));
 		Image img = new Image(0, imageId.toString(),  thumbId.toString(), description);
+		img.setVideoGame(g.get());
 		return new ResponseEntity<>(imageRepository.save(img), HttpStatus.CREATED);
 	}
+
 	
+	@GetMapping("/data")
+	@ResponseBody
+	public ResponseEntity<InputStreamResource> getFile(@RequestParam("id") int id) throws IllegalStateException, IOException {
+		Optional<Image> img = imageRepository.findById(id);
+		if (!img.isPresent())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			
+		GridFSFile fileInfos = this.gridFsTemplate.findOne(
+				new Query(Criteria.where("_id").is(
+						img.get().getStorageId()
+					)));
+		
+		if (fileInfos == null)
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		GridFsResource resource = this.gridFsTemplate.getResource(fileInfos);
+		InputStreamResource binaryData = new InputStreamResource(
+				resource.getInputStream());
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		// le content type
+		headers.setContentType(MediaType.parseMediaType(resource.getContentType()));
+		// la taille du fichier
+		headers.setContentLength(resource.contentLength());
+		// le nom du fichier original
+		headers.set("Content-Disposition", "attachment; filename="+ resource.getFilename());
+		
+		return new ResponseEntity<InputStreamResource>(binaryData, headers, HttpStatus.OK);
+		
+	}
 	
-	
-//	
-//	@GetMapping("/{id:[0-9]+}/data")
-//	public ResponseEntity<FileSystemResource> imageData(@PathVariable("id")int id) {
-//		Optional<Image> op = imageRepository.findById(id);
-//		if (!op.isPresent())
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		Optional<File> ofile = imageRepository.getImageFile(
-//										op.get().getStorageId());
-//		if (!ofile.isPresent())
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.parseMediaType(op.get().getContentType()));
-//		headers.setContentLength(ofile.get().length());
-//		headers.setContentDispositionFormData("attachment", op.get().getFileName());
-//		ResponseEntity<FileSystemResource> re = 
-//			new ResponseEntity<FileSystemResource>(
-//				new FileSystemResource(ofile.get()),
-//				headers,
-//				HttpStatus.OK);
-//		return re;
-//	}
-//	
-//	@GetMapping("/{id:[0-9]+}/thumbdata")
-//	public ResponseEntity<FileSystemResource> imageThumbData(@PathVariable("id")int id) {
-//		Optional<Image> op = imageRepository.findById(id);
-//		if (!op.isPresent())
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		Optional<File> ofile = imageRepository.getImageFile(
-//										op.get().getThumbStorageId());
-//		if (!ofile.isPresent())
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.IMAGE_JPEG);
-//		headers.setContentLength(ofile.get().length());
-//		headers.setContentDispositionFormData("attachment", op.get().getFileName());
-//		ResponseEntity<FileSystemResource> re = 
-//			new ResponseEntity<FileSystemResource>(
-//				new FileSystemResource(ofile.get()),
-//				headers,
-//				HttpStatus.OK);
-//		return re;
-//	}	
+	@GetMapping("/thumbdata")
+	@ResponseBody
+	public ResponseEntity<InputStreamResource> getThumbdata(@RequestParam("id") int id) throws IllegalStateException, IOException {
+		Optional<Image> img = imageRepository.findById(id);
+		if (!img.isPresent())
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			
+		GridFSFile fileInfos = this.gridFsTemplate.findOne(
+				new Query(Criteria.where("thumb_").is(
+						img.get().getStorageId()
+					)));
+		
+		if (fileInfos == null)
+			return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+		
+		GridFsResource resource = this.gridFsTemplate.getResource(fileInfos);
+		InputStreamResource binaryData = new InputStreamResource(
+				resource.getInputStream());
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		// le content type
+		headers.setContentType(MediaType.parseMediaType(resource.getContentType()));
+		// la taille du fichier
+		headers.setContentLength(resource.contentLength());
+		// le nom du fichier original
+		headers.set("Content-Disposition", "attachment; filename="+ resource.getFilename());
+		
+		return new ResponseEntity<InputStreamResource>(binaryData, headers, HttpStatus.OK);
+		
+	}
+
+	//	
+	//	@GetMapping("/{id:[0-9]+}/thumbdata")
+	//	public ResponseEntity<FileSystemResource> imageThumbData(@PathVariable("id")int id) {
+	//		Optional<Image> op = imageRepository.findById(id);
+	//		if (!op.isPresent())
+	//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	//		Optional<File> ofile = imageRepository.getImageFile(
+	//										op.get().getThumbStorageId());
+	//		if (!ofile.isPresent())
+	//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	//		HttpHeaders headers = new HttpHeaders();
+	//		headers.setContentType(MediaType.IMAGE_JPEG);
+	//		headers.setContentLength(ofile.get().length());
+	//		headers.setContentDispositionFormData("attachment", op.get().getFileName());
+	//		ResponseEntity<FileSystemResource> re = 
+	//			new ResponseEntity<FileSystemResource>(
+	//				new FileSystemResource(ofile.get()),
+	//				headers,
+	//				HttpStatus.OK);
+	//		return re;
+	//	}	
 
 }
